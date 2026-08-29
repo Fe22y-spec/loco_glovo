6f78862a2ea2d5fcac6d847c3bf246ffcbbb7ab2
dev F3stus <devfestusomerikit@gmail.com>
2026-08-29 15:45:24 +0300

    changed to correct supabase anon key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://rwevslisctwexarloeqk.supabase.co";
 const supabaseAnonKey =
-  "sb_publishable_Bs9p3KO7MUOescWQ2vZpAA_c6I9qM_I";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3ZXZzbGlzY3R3ZXhhcmxvZXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMzg5MTYsImV4cCI6MjEwMDcxNDkxNn0.ocMGBG7b1Bkrf8gOX8hHH29zxJ7v5df_HWbvzac5f-M";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
